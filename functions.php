b0642bc481fc16081c5949f15534bb25656225fd
soup-bowl <code@soupbowl.io>
2020-06-13 14:15:29 +0100

    Establishing colour overrides.

--- functions.php
+++ functions.php
@@ -7,3 +7,9 @@ add_action(
 		wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css', [], '1.0' );
 	}
 );
+
+// Kill off twentytwenty's colour selection.
+function sbat_remove_colour( $wp_customize ) {
+	$wp_customize->remove_section( 'colors' );
+}
+add_action( 'customize_register', 'sbat_remove_colour' );
