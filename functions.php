bfa90f843d0bf95858bd3a9f7e6f0ada3f6790d9
soup-bowl <code@soupbowl.io>
2020-06-14 04:39:07 +0100

    Inc. ver.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_action(
 	'wp_enqueue_scripts',
 	function() {
-		wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css', [], '1.0' );
+		wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css', [], '1' );
 	}
 );
 
