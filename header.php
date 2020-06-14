8d427fe89ae0e39f858fdb415bd44703908a3882
soup-bowl <code@soupbowl.io>
2020-06-14 12:28:51 +0100

    Menu fix

--- header.php
+++ header.php
@@ -52,7 +52,6 @@
 								<span class="toggle-icon">
 									<?php twentytwenty_the_theme_svg( 'search' ); ?>
 								</span>
-								<span class="toggle-text"><?php _e( 'Search', 'twentytwenty' ); ?></span>
 							</span>
 						</button><!-- .search-toggle -->
 
@@ -76,7 +75,6 @@
 							<span class="toggle-icon">
 								<?php twentytwenty_the_theme_svg( 'ellipsis' ); ?>
 							</span>
-							<span class="toggle-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
 						</span>
 					</button><!-- .nav-toggle -->
 
@@ -157,7 +155,6 @@
 								<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
 									<span class="toggle-inner">
 										<?php twentytwenty_the_theme_svg( 'search' ); ?>
-										<span class="toggle-text"><?php _e( 'Search', 'twentytwenty' ); ?></span>
 									</span>
 								</button><!-- .search-toggle -->
 
